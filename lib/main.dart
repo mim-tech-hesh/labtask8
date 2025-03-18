dd8da869e1a1ac5725d69f6e588c7067c7e2f72c
mim-hesh-tech <mim15-5924@diu.edu.bd>
2025-03-18 10:33:07 +0600

    screen color change

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,99 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Mega shop',
-      firstIcon:(Iconsax.notification_bimg_outline, [
-      ),//FAppBar
-      BottomNavigationBarItem(
-        icon:Icon(Iconsax.home_outline),
-        label:"HOME"
-      ),// BottomNavigationBarItem
-      BottomNavigationBarItem(
-        icon:Icon(
-          Iconsax.heart_outline,
-         ), //Icon
-         label:"WISHLIST"
-       ),BottomNavigationBarItem
-       BottomNavigationBarItem(
-        icon:Icon(
-          Iconsax.heart_outline,
-        ),
-        label:"WISHLIST"),
-        BottomNavigationBarItem(
-          icon:Icon(
-            Iconsax.shopping_bag_outline,
-          ),
-          label:"ORDER"),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreenContent(),
+    WishlistScreenContent(),
+    OrderScreenContent(),
+    AccountScreenContent(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon:Icon(applyTextScaling: Iconsax.user_outline,
-            ),
-            label:"ACCOUNT"),
-    ]), //BottomNavigationbar
-    );Scaffold
+            icon: Icon(Icons.home),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'WISHLIST',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'ORDER',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'ACCOUNT',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
 
+class HomeScreenContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.yellow);
+  }
+}
 
+class WishlistScreenContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
+  }
+}
 
+class OrderScreenContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.green);
+  }
+}
 
-     
+class AccountScreenContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue);
+  }
+}
+
